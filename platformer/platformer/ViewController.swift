f4f64b1caaee4670fafcccf2bcf34abb5d3600e7
Christopher Williams <chrsw49@gmail.com>
2015-10-18 11:49:56 -0700

    start on 2D platformer, not working well

--- platformer/platformer/ViewController.swift
+++ platformer/platformer/ViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var test: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        test.center.x = view.bounds.width / 2
+        test.center.y = view.bounds.height / 2
+        test.backgroundColor = UIColor.redColor()
     }
 
     override func didReceiveMemoryWarning() {
