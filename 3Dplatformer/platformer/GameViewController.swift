2a86393b5ececd2a7a3e68a2c8c9a4f5acb339a2
Christopher Williams <chrsw49@gmail.com>
2015-10-18 11:49:33 -0700

    hacky box jumping animation

--- 3Dplatformer/platformer/GameViewController.swift
+++ 3Dplatformer/platformer/GameViewController.swift
@@ -24,7 +24,7 @@ class GameViewController: UIViewController {
         scene.rootNode.addChildNode(cameraNode)
         
         // place the camera
-        cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
+        cameraNode.position = SCNVector3(x: 0, y: 5, z: 15)
         
         // create and add a light to the scene
         let lightNode = SCNNode()
@@ -61,50 +61,35 @@ class GameViewController: UIViewController {
         // configure the view
         scnView.backgroundColor = UIColor.blackColor()
         
-//        // add a tap gesture recognizer
-//        let tapGesture = UITapGestureRecognizer(target: self, action: "handleTap:")
-//        var gestureRecognizers = [UIGestureRecognizer]()
-//        gestureRecognizers.append(tapGesture)
-//        if let existingGestureRecognizers = scnView.gestureRecognizers {
-//            gestureRecognizers.appendContentsOf(existingGestureRecognizers)
-//        }
-//        scnView.gestureRecognizers = gestureRecognizers
+        // add a tap gesture recognizer
+        let tapGesture = UITapGestureRecognizer(target: self, action: "handleTap:")
+        var gestureRecognizers = [UIGestureRecognizer]()
+        gestureRecognizers.append(tapGesture)
+        if let existingGestureRecognizers = scnView.gestureRecognizers {
+            gestureRecognizers.appendContentsOf(existingGestureRecognizers)
+        }
+        scnView.gestureRecognizers = gestureRecognizers
     }
     
-//    func handleTap(gestureRecognize: UIGestureRecognizer) {
-//        // retrieve the SCNView
-//        let scnView = self.view as! SCNView
-//        
-//        // check what nodes are tapped
-//        let p = gestureRecognize.locationInView(scnView)
-//        let hitResults = scnView.hitTest(p, options: nil)
-//        // check that we clicked on at least one object
-//        if hitResults.count > 0 {
-//            // retrieved the first clicked object
-//            let result: AnyObject! = hitResults[0]
-//            
-//            // get its material
-//            let material = result.node!.geometry!.firstMaterial!
-//            
-//            // highlight it
-//            SCNTransaction.begin()
-//            SCNTransaction.setAnimationDuration(0.5)
-//            
-//            // on completion - unhighlight
-//            SCNTransaction.setCompletionBlock {
-//                SCNTransaction.begin()
-//                SCNTransaction.setAnimationDuration(0.5)
-//                
-//                material.emission.contents = UIColor.blackColor()
-//                
-//                SCNTransaction.commit()
-//            }
-//            
-//            material.emission.contents = UIColor.redColor()
-//            
-//            SCNTransaction.commit()
-//        }
-//    }
+    func handleTap(gestureRecognize: UIGestureRecognizer) {
+        // retrieve the SCNView
+        let scnView = self.view as! SCNView
+        
+        // retrieve box object
+        let box = scnView.scene!.rootNode.childNodeWithName("box", recursively: true)!
+        
+        // movement variables
+        let time = 0.3
+        let speed : CGFloat = 5
+        
+        // hacky jumping animation. TODO use CABasicAnimation?
+        if box.position.y <= 0 {
+            box.runAction(SCNAction.moveByX(0, y: speed, z: 0, duration: time), completionHandler: {
+                () in
+                box.runAction(SCNAction.moveByX(0, y: -speed, z: 0, duration: time))
+            })
+        }
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
