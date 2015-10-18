18135a89c2477250bd5f0897543729897ac844d6
Christopher Williams <chrsw49@gmail.com>
2015-10-18 13:16:04 -0700

    positioned title at middle top and player at middle bottom

--- platformer/platformer/GameScene.swift
+++ platformer/platformer/GameScene.swift
@@ -10,33 +10,39 @@ import SpriteKit
 
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+        // title at top middle
+        let title = SKLabelNode(fontNamed:"Chalkduster")
+        title.text = "Platformer"
+        title.fontSize = 65
+        title.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMaxY(self.frame) - 150)
+        self.addChild(title)
         
-        self.addChild(myLabel)
+        // player at middle-bottom of screen
+        let sprite = SKSpriteNode(imageNamed:"Spaceship")
+        sprite.xScale = 0.2
+        sprite.yScale = 0.2
+        sprite.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMinY(self.frame) + 150)
+        self.addChild(sprite)
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        /* Called when a touch begins */
-        
-        for touch in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
+        // TODO make ship jump
+//        
+//        for touch in touches {
+//            let location = touch.locationInNode(self)
+//            
+//            let sprite = SKSpriteNode(imageNamed:"Spaceship")
+//            
+//            sprite.xScale = 0.5
+//            sprite.yScale = 0.5
+//            sprite.position = location
+//            
+//            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
+//            
+//            sprite.runAction(SKAction.repeatActionForever(action))
+//            
+//            self.addChild(sprite)
+//        }
     }
    
     override func update(currentTime: CFTimeInterval) {
