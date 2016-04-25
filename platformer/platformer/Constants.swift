3df29bf1a8044c159c1a26c68310de1999ca2569
Christopher Williams <chrsw49@gmail.com>
2016-04-24 21:58:14 -0700

    flying enemies level

--- platformer/platformer/Constants.swift
+++ platformer/platformer/Constants.swift
@@ -42,9 +42,10 @@ struct Constants {
     static let CollisionCategory_Goal:UInt32 = 0x1 << 3
     
     // World Levels
-    static let LevelSpawnPoints = [CGPointMake(0, 240),
-                                   CGPointMake(12200, 768),
-                                   CGPointMake(16600, 240),
-                                   CGPointMake(6600, 240),
-                                   CGPointMake(22336, 240)]
+//    static let LevelSpawnPoints = [CGPointMake(0, 240),
+//                                   CGPointMake(12200, 768),
+//                                   CGPointMake(16600, 240),
+//                                   CGPointMake(6600, 240),
+//                                   CGPointMake(22336, 240)]
+    static let LevelSpawnPoints = [CGPointMake(6600, 240)]
 }
